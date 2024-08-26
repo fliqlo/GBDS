15199c77ddb3c661a11005efb23c9dff6f2d0069
fliqlo <128495492+fliqlo@users.noreply.github.com>
2024-08-26 16:47:09 +0800

    Add files via upload

---
+++ GBDS_LOE.py
@@ -0,0 +1,42 @@
+#GBDS LIST OF EMPLOYEES
+
+import os
+
+Employee_Names = ["Danoy, Christine F.", "Cabales, Wilfredo Jr D.", "Dacula, Paquiton A.", "Salig, Juliven D.", "Arances, Mario T.", "Enclonar, Jerome R.",
+				  "Buason, Nelboy L.", "Bali-ot, Reyden D.", "Salig, Jovani D.", "Marco, Brian Geoffrey D.", "Talledo, Sarah Jane C.", "Gica, Grace B.", 
+				  "Fuentes, John Michael K.", "Chiong, Rie D."]
+Employee_Type = ["DSP/Dealer Sales Personal", "Driver", "Truckman", "Encoder", "Checker", "Sales Supervisor", "Dealer"]
+Contact_Number = ["0905-587-0882", "0965-773-1719", "0953-748-8427", "0953-990-4443", "0955-297-1524", "0905-679-6298", "0965-835-3320",
+				  "0916-845-9318", "0995-033-6733", "0917-474-9979", "0936-460-9704", "0951-468-3629"]
+Current_Address = ["Sindangan, Sto. Rosario, ZdN", "Sindangan, Lawis, ZdN", "Sindangan, Upper Inuman, ZdN", "Sindangan, Pangalalan, ZdN", "Siayan, Poblacion, ZdN",
+				   "Sindangan, Lower Inuman, ZdN", "Sindangan, Lagag, ZdN", "Sindangan, Disud, ZdN", "Sindangan, Lower Inuman, ZdN", "Sindangan, Siare, ZdN",
+				   "Liloy, Poblacion, ZdN", "Sindangan, Poblacion, ZdN"]
+Birth_Date = ["November 14, 1998", "June 20, 1995", "February 24, 1992", "May 05, 1994", "February 02, 1981", "September 22, 1995", "October 17, 2000", "February 20, 1996",
+			  "April 01, 1979", "February 26, 1988", "December 02, 1998"]
+Employment_Status = ["Regular"]
+Date_Hired = ["Thursday, August 15, 2024"]
+
+
+os.system("cls")
+print("")
+print("##########################")
+print("# GBDS LIST OF EMPLOYEES #")
+print("##########################")
+print("__________________________________________________________________________________________________________________________________________________________________________________________________")
+print("Names                    ", "|Employee Type             ", "|Contact Number           ", "|Current Address               ", "|Birth Date           ", "|Employment Status           ", "|Job Started                |")
+print("-------- -----------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*")
+print(Employee_Names[0] + "       |" + Employee_Type[0] + "  |" + Contact_Number[0] + "             |" + Current_Address[0] + "   |" + Birth_Date[0] + "     |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[1] + "   |" + Employee_Type[0] + "  |" + Contact_Number[1] + "             |" + Current_Address[1] + "          |" + Birth_Date[1] + "         |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[2] + "       |" + Employee_Type[0] + "  |" + Contact_Number[2] + "             |" + Current_Address[2] + "   |" + Birth_Date[2] + "     |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[3] + "         |" + Employee_Type[1] + "                     |" + Contact_Number[3] + "             |" + Current_Address[6] + "          |" + Birth_Date[3] + "          |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[4] + "         |" + Employee_Type[1] + "                     |" + Contact_Number[4] + "             |" + Current_Address[5] + "   |" + Birth_Date[4] + "     |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[5] + "       |" + Employee_Type[1] + "                     |" + Contact_Number[5] + "             |" + Current_Address[3] + "     |" + "                      |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[6] + "         |" + Employee_Type[2] + "                   |" + Contact_Number[6] + "             |" + Current_Address[6] + "          |" + Birth_Date[5] + "    |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[7] + "        |" + Employee_Type[2]  + "                   |" + Contact_Number[7] + "             |" + Current_Address[3] + "     |" + "                      |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[8] + "          |" + Employee_Type[2] + "                   |" + Contact_Number[8] + "             |" + Current_Address[6] + "          |" + "                      |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[9] + "  |" + Employee_Type[3] + "                    |" + "?" + "                         |" + Current_Address[7] + "          |" + Birth_Date[6] + "      |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[10] + "    |" + Employee_Type[4] + "                    |" + Contact_Number[10] + "             |" + Current_Address[8] + "   |" + Birth_Date[7] + "     |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[11] + "            |" + Employee_Type[4] + "                    |" + Contact_Number[11] + "             |" + Current_Address[9] + "          |" + Birth_Date[8] + "        |" + Employment_Status[0] + "                      |" + Date_Hired[0])
+print(Employee_Names[12] + "  |" + Employee_Type[5] + "           |" + "?" + "                         |" + Current_Address[10] + "          |" + Birth_Date[9] + "     |" + "                             |")
+print(Employee_Names[13] + "            |" + Employee_Type[6] + "                     |" + "?" + "                         |" + Current_Address[11] + "      |" + Birth_Date[10] + "     |" + "                             |")
+print("")
