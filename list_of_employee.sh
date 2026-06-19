006ee3715a4b57bcfe47d29efa13f75f2eae2625
fliqlo <128495492+fliqlo@users.noreply.github.com>
2026-06-19 11:52:21 +0800

    Add employee listing script for GBDS

---
+++ list_of_employee.sh
@@ -0,0 +1,99 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+
+echo "Guillermo Beverage Distribution Services"
+echo "List of employee"
+echo ""
+
+#RT1
+dsp1="Dandayo,"
+dsp1_name="Ronnel"
+
+drv1="Salig,"
+drv1_name="Juliven"
+
+trkman1="Baoc,"
+trkman1_name="Precillano"
+
+echo "Route 1
+Dsp: $dsp1 $dsp1_name
+Driver: $drv1 $drv1_name
+Truckman: $trkman1 $trkman1_name
+";
+
+#RT2
+dsp2="Pormento,"
+dsp2_name="Estemark"
+
+drv2="Enclonar,"
+drv2_name="Jerome"
+
+trkman2="Salig,"
+trkman2_name="Jovani"
+
+echo "Route 2
+Dsp: $dsp2 $dsp2_name
+Driver: $drv2 $drv2_name
+Truckman: $trkman2 $trkman2_name
+";
+
+#RT3
+dsp3="Gica,"
+dsp3_name="Grace"
+
+drv3="Gomora,"
+drv3_name="Jurlan"
+
+trkman3="Racaza,"
+trkman3_name="Alexander"
+
+echo "Route 3
+Dsp: $dsp3 $dsp3_name
+Driver: $drv3 $drv3_name
+Truckman: $trkman3 $trkman3_name
+";
+
+#Warehouse
+chckr="Talledo,"
+chckr_name="Sarah Jane"
+
+frklft_optr="Andapo,"
+frklft_optr_name="John"
+
+hlpr="Podiotan,"
+hlpr_name="Air John"
+
+echo "Warehoue
+Checker: $chckr $chckr_name
+Forklift Operator: $frklft_optr $frklft_optr_name
+Helper: $hlpr $hlpr_name
+";
+
+#Office
+encdr="Dontar,"
+encdr_name="Berlyn"
+
+echo "Office
+Encoder: $encdr $encdr_name
+";
+
+#Dealer
+dealer="Chiong,"
+dealer_name="Rie"
+
+echo "Dealer
+Dealer: $dealer $dealer_name
+";
+
+#Supervisor
+supervisor="Baguio,"
+supervisor_name="Neilvin"
+
+echo "Supervisor
+Supervisor: $supervisor $supervisor_name
+";
+
+echo "All rights reserved. GBDS 2026.";
+
+
